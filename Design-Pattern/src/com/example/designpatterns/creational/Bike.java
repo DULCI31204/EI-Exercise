07089322cbbe4d64d2bced2755b3d700f0bd782d
DULCI31204 <sdulcieugiyana@gmail.com>
2025-09-28 13:04:18 +0530

    Bike.java

---
+++ Design-Pattern/src/com/example/designpatterns/creational/Bike.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.designpatterns.creational;
+
+/**
+ *
+ * @author sdulc
+ */
+public class Bike implements Vehicle {
+    @Override
+    public String drive() {
+        return "Riding a bike";
+    }
+}
